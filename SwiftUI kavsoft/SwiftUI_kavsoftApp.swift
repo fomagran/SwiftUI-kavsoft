760292b75c16c699acc848ca932ab2eed4ef47f1
Fomagran <fomagran6@naver.com>
2022-03-27 16:51:10 -0500

    Initial Commit

---
+++ SwiftUI kavsoft/SwiftUI_kavsoftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_kavsoftApp.swift
+//  SwiftUI kavsoft
+//
+//  Created by Fomagran on 2022/03/27.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_kavsoftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
